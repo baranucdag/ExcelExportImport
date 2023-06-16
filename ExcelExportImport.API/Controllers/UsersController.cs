d0e74f31a9554b859f0110ac443bfcb5d3c24db2
baran.ucdag <baran.ucdag@WRPNW060>
2023-06-16 17:11:00 +0300

    Add project files.

---
+++ ExcelExportImport.API/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using ExcelExportImport.API.Entities;
+using ExcelExportImport.API.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExcelExportImport.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+        [HttpGet("GetExcelList")]
+        public IActionResult GetExcelList()
+        {
+
+            List<User> userList = new List<User>
+            {
+                new User {Id=Guid.NewGuid(), FirstName="Baran",LastName="Üçdağ",Email="baran.ucdag@warpiris.com",PasswordHash="Passowrd",PasswordSalt="Salt" ,Status=true},
+                new User {Id=Guid.NewGuid(), FirstName="Baran",LastName="Üçdağ",Email="baran.ucdag@warpiris.com",PasswordHash="Passowrd",PasswordSalt="Salt" ,Status=true},
+                new User {Id=Guid.NewGuid(), FirstName="Baran",LastName="Üçdağ",Email="baran.ucdag@warpiris.com",PasswordHash="Passowrd",PasswordSalt="Salt" ,Status=true},
+                new User {Id=Guid.NewGuid(), FirstName="Baran",LastName="Üçdağ",Email="baran.ucdag@warpiris.com",PasswordHash="Passowrd",PasswordSalt="Salt" ,Status=true},
+                new User {Id=Guid.NewGuid(), FirstName="Baran",LastName="Üçdağ",Email="baran.ucdag@warpiris.com",PasswordHash="Passowrd",PasswordSalt="Salt" ,Status=true}
+            };
+
+            Stream excelStream = ExcelProcessService.ExportToExcel(userList);
+
+
+            // Excel dosyasını indirmek için gerekli adımlar
+            byte[] excelBytes = ((MemoryStream)excelStream).ToArray();
+
+
+            return Ok(File(excelBytes, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"));
+        }
+    }
+}
---
+++ ExcelExportImport.API/Service/ExcelProcessService.cs
@@ -0,0 +1,54 @@
+﻿using ClosedXML.Excel;
+
+namespace ExcelExportImport.API.Service
+{
+    public class ExcelProcessService
+    {
+
+        /// <summary>
+        /// Verilen listeyi excel çıktısına dönüştüren mettotur.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="data"></param>
+        public static Stream ExportToExcel<T>(List<T> data)
+        {
+            // Excel çalışma kitabını oluştur
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Sheet1");
+
+                // Başlık satırını ekleyin ve stilleri uygular
+                int columnIndex = 1;
+                var headerRow = worksheet.Row(1);
+                headerRow.Style.Font.Bold = true;
+                headerRow.Style.Fill.BackgroundColor = XLColor.LightGray;
+                foreach (var property in typeof(T).GetProperties())
+                {
+                    worksheet.Cell(1, columnIndex).Value = property.Name;
+                    columnIndex++;
+                }
+
+                // Verileri satırlara ekler
+                int rowIndex = 2;
+                foreach (var item in data)
+                {
+                    columnIndex = 1;
+                    foreach (var property in typeof(T).GetProperties())
+                    {
+                        worksheet.Cell(rowIndex, columnIndex).Value = property.GetValue(item).ToString();
+                        columnIndex++;
+                    }
+                    rowIndex++;
+                }
+
+                // Excel dosyasını bellek akışına kaydeder
+                using (var memoryStream = new MemoryStream())
+                {
+                    workbook.SaveAs(memoryStream);
+                    memoryStream.Position = 0;
+                    return memoryStream;
+                }
+            }
+        }
+    }
+}
